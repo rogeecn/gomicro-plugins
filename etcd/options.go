94adc84d1a6c9412b282f7b8751a1f6526d6b701
rogeecn <rogeecn@qq.com>
2023-08-04 20:18:50 +0800

    init

---
+++ etcd/options.go
@@ -0,0 +1,32 @@
+package etcd
+
+import (
+	"context"
+
+	"go-micro.dev/v4/registry"
+	clientv3 "go.etcd.io/etcd/client/v3"
+	"go.uber.org/zap"
+)
+
+type clientKey struct{}
+
+type logConfigKey struct{}
+
+func Client(client clientv3.Client) registry.Option {
+	return func(o *registry.Options) {
+		if o.Context == nil {
+			o.Context = context.Background()
+		}
+		o.Context = context.WithValue(o.Context, clientKey{}, client)
+	}
+}
+
+// LogConfig allows you to set etcd log config.
+func LogConfig(config *zap.Config) registry.Option {
+	return func(o *registry.Options) {
+		if o.Context == nil {
+			o.Context = context.Background()
+		}
+		o.Context = context.WithValue(o.Context, logConfigKey{}, config)
+	}
+}
